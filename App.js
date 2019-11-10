518b23beda990b80186531f86e68eafa074d324e
José Arthur <zearthur99@Joses-MacBook-Air.local>
2019-11-10 15:30:32 -0300

    feat: Estilo de abas e sombramento implementado

--- App.js
+++ App.js
@@ -2,29 +2,42 @@ import React from 'react';
 import { KeyboardAvoidingView, StyleSheet } from 'react-native';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { PRIMARY_COLOR, GREEN } from './src/utils/colors';
-import TopStatusBar from './src/components/StatusBar';
+import { loadAsync } from 'expo-font';
+import { PRIMARY } from './src/utils/colors';
 import HomeHeader from './src/components/HomeHeader';
-import { AppTabs } from './src/navigation';
 import reducer from './src/reducer';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: PRIMARY_COLOR,
+    backgroundColor: PRIMARY,
     justifyContent: 'center',
     textAlign: 'center',
   },
 });
 
 export default function() {
+  const { useEffect, useState } = React;
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  const loadFont = async () => {
+    await loadAsync({
+      'varela-round': require('./assets/VarelaRound-Regular.ttf'),
+    });
+    setFontLoaded(true);
+  };
+
+  useEffect(() => {
+    loadFont();
+  });
+
   return (
     <Provider store={createStore(reducer)}>
-      <KeyboardAvoidingView behavior="padding" style={styles.container}>
-        <TopStatusBar backgroundColor={GREEN} barStyle="light-content" />
-        <HomeHeader />
-        <AppTabs />
-      </KeyboardAvoidingView>
+      {fontLoaded && (
+        <KeyboardAvoidingView behavior="padding" style={styles.container}>
+          <HomeHeader />
+        </KeyboardAvoidingView>
+      )}
     </Provider>
   );
 }
--- src/components/HomeHeader/index.js
+++ src/components/HomeHeader/index.js
@@ -1,33 +1,43 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { RED, WHITE, GREEN } from '../../utils/colors';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
+import { WHITE, PRIMARY, PRIMARY_DARK } from '../../utils/colors';
+import { elevationShadowStyle } from '../../utils/shadows';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 2,
-    padding: 20,
-    color: WHITE,
-    backgroundColor: RED,
-    alignSelf: 'flex-start',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-    textAlign: 'center',
+  tabsWrapper: {
+    flexDirection: 'row',
   },
-  center: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    textAlign: 'center',
-    fontSize: 20,
+  text: {
+    alignSelf: 'center',
     color: WHITE,
+    fontSize: 22,
+    fontFamily: 'varela-round',
+  },
+  headerContentWrapper: {
+    paddingBottom: 18,
   },
   header: {
-    position: 'relative',
-    backgroundColor: GREEN,
-    height: 80,
-    left: 0,
-    right: 0,
-    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: PRIMARY,
+    ...elevationShadowStyle(30),
+  },
+  tab: {
+    paddingVertical: 14,
+    paddingHorizontal: 14,
+  },
+  activeTab: {
+    borderStyle: 'solid',
+    borderBottomWidth: 3,
+    borderBottomColor: WHITE,
+  },
+  tabText: {
+    textTransform: 'uppercase',
+    fontSize: 14,
+    color: PRIMARY_DARK,
+    fontFamily: 'varela-round',
+  },
+  activeTabText: {
     color: WHITE,
   },
 });
@@ -35,10 +45,21 @@ const styles = StyleSheet.create({
 export default function HomeHeader() {
   return (
     <View style={styles.header}>
-      <Text style={[styles.center, { paddingTop: 5, alignSelf: 'center' }]}>
-        JOSÉ ARTHUR SILVEIRA
-      </Text>
-      <Text style={[styles.center, { paddingTop: 5, alignSelf: 'center' }]}>201713009-1</Text>
+      <View style={styles.headerContentWrapper}>
+        <Text style={[styles.text]}>José Arthur Silveira</Text>
+        <Text style={[styles.text, { fontSize: 12 }]}>201710712-9</Text>
+      </View>
+      <ScrollView horizontal style={styles.tabsWrapper}>
+        <View style={[styles.tab, styles.activeTab]}>
+          <Text style={[styles.tabText, styles.activeTabText]}>Período atual</Text>
+        </View>
+        <View style={styles.tab}>
+          <Text style={styles.tabText}>Disciplinas</Text>
+        </View>
+        <View style={styles.tab}>
+          <Text style={styles.tabText}>Meus dados</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
